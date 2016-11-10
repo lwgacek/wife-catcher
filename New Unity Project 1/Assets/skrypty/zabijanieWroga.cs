a27d63d4ce0c621c6a6a3fec6a2232159c078aa2
lwgacek <lgiecewicz@go2.pl>
2016-11-10 19:11:48 +0100

    poprawiona animacja Time PowerUpa

--- New Unity Project 1/Assets/skrypty/zabijanieWroga.cs
+++ New Unity Project 1/Assets/skrypty/zabijanieWroga.cs
@@ -4,8 +4,9 @@ public class zabijanieWroga : MonoBehaviour {
 
 	public int punkty = 1;
 
-	void OnTriggerEnter2D(){
-
+	void OnTriggerEnter2D (Collider2D col)
+    {
+            if( col.gameObject.tag == "Player" )
 		Destroy(gameObject);
 		CzasiWynik.score += punkty;
 		if (CzasiWynik.score > CzasiWynik.hscore)
