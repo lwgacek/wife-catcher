c3293fcd31753f47eb2a1cab8027c8437824de43
lwgacek <lgiecewicz@go2.pl>
2016-11-10 18:21:53 +0100

    dodana animacja Time PowrUpa

--- New Unity Project 1/Assets/skrypty/czasPowrerupZbieranie.cs
+++ New Unity Project 1/Assets/skrypty/czasPowrerupZbieranie.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class czasPowrerupZbieranie : MonoBehaviour {
 
+        Animation animacja;
+
+    void Start()
+    {
+        animacja = GetComponent<Animation>();
+        
+    }
     void OnCollisionEnter2D(Collision2D col)
         {
         if (col.gameObject.tag == "Player");
         Destroy(gameObject);
         }
+    void Update()
+    {
+        animacja.Play("timepowerup");
+    }
 }
